222de789fbfc2e9e75948d278b68ef97b35a96fa
akshpujari <aksh.pujari@gmail.com>
2023-08-17 21:32:31 +0530

    Fixed Issue Related Pagination

--- src/components/movie-list/movie-list.js
+++ src/components/movie-list/movie-list.js
@@ -28,6 +28,7 @@ function MovieList() {
     })
 
     const onSearchChange = ((movieName) => {
+        setPageNumber(1)
         if (movieName.target.value) {
             axios.get(`https://api.themoviedb.org/3/search/movie?query=${movieName.target.value}&page=1&api_key=c2ce9c4b90b2697d2021ce5ee7aff683`)
                 .then(response => {
@@ -46,18 +47,29 @@ function MovieList() {
         getMovies();
         inputRef.current.value = null;
         console.log(inputRef, "input")
+        setPageNumber(1)
 
     })
 
     const onLoadMore = (() => {
         console.log(inputRef.current.value)
-        axios.get(`https://api.themoviedb.org/3/discover/movie?query=${inputRef.current.value}&page=${pageNumber + 1}&api_key=c2ce9c4b90b2697d2021ce5ee7aff683`)
-            .then(response => {
-                setMovies((oldMovies) => [...oldMovies, ...response.data.results])
-            })
-            .catch(error => {
-                console.error(error);
-            });
+        if (inputRef.current.value) {
+            axios.get(`https://api.themoviedb.org/3/search/movie?query=${inputRef.current.value}&page=${pageNumber + 1}&api_key=c2ce9c4b90b2697d2021ce5ee7aff683`)
+                .then(response => {
+                    setMovies((oldMovies) => [...oldMovies, ...response.data.results])
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        } else {
+            axios.get(`https://api.themoviedb.org/3/discover/movie?page=${pageNumber + 1}&api_key=c2ce9c4b90b2697d2021ce5ee7aff683`)
+                .then(response => {
+                    setMovies((oldMovies) => [...oldMovies, ...response.data.results])
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
         setPageNumber((num) => num + 1)
     })
 
@@ -82,46 +94,48 @@ function MovieList() {
             </AppBar>
             <Box height="5rem"></Box>
 
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                {movies.map(movie => (
-                    <Grid item key={movie.id} >
-                        <Link to={`${movie.id}/movie-detail`} style={{ textDecorationLine: "none" }}>
-                            <Card sx={{ maxWidth: 250 }} >
-                                <CardActionArea >
-                                    <CardMedia>
-                                        <img src={(`https://image.tmdb.org/t/p/original${movie.poster_path}`)} style={{ width: '100%' }} />
-                                    </CardMedia>
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h6" component="div" style={{ height: 65 }}>
-                                            {movie.title}
-                                        </Typography>
-                                        <div>
-                                            {movie.vote_average ?
-                                                (<StarRatings
-                                                    rating={movie.vote_average / 2}
-                                                    starDimension="15px"
-                                                    starSpacing="5px"
-                                                    starRatedColor="#FF9529"
-                                                />
-                                                ) : ''}
-                                        </div>
-                                        <Typography variant="body2" color="text.secondary" sx={{
-                                            overflow: "hidden",
-                                            textOverflow: "ellipsis",
-                                            display: "-webkit-box",
-                                            WebkitLineClamp: "2",
-                                            WebkitBoxOrient: "vertical",
-                                        }}>
-                                            {movie.overview}
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
-                        </Link>
-                    </Grid>
-                ))}
-            </Grid>
-
+            {movies ?
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    {movies.map(movie => (
+                        <Grid item key={movie.id} >
+                            <Link to={`${movie.id}/movie-detail`} style={{ textDecorationLine: "none" }}>
+                                <Card sx={{ maxWidth: 250 }} >
+                                    <CardActionArea >
+                                        <CardMedia>
+                                            <img src={(`https://image.tmdb.org/t/p/original${movie.poster_path}`)} style={{ width: '100%' }} />
+                                        </CardMedia>
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h6" component="div" style={{ height: 65 }}>
+                                                {movie.title}
+                                            </Typography>
+                                            <div>
+                                                {movie.vote_average ?
+                                                    (<StarRatings
+                                                        rating={movie.vote_average / 2}
+                                                        starDimension="15px"
+                                                        starSpacing="5px"
+                                                        starRatedColor="#FF9529"
+                                                    />
+                                                    ) : ''}
+                                            </div>
+                                            <Typography variant="body2" color="text.secondary" sx={{
+                                                overflow: "hidden",
+                                                textOverflow: "ellipsis",
+                                                display: "-webkit-box",
+                                                WebkitLineClamp: "2",
+                                                WebkitBoxOrient: "vertical",
+                                            }}>
+                                                {movie.overview}
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            </Link>
+                        </Grid>
+                    ))}
+                </Grid>
+                : <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>Loading...</div>
+            }
             <div className={styles.loadMoreButton}>
                 <Button variant="contained" onClick={onLoadMore}>Load more</Button>
             </div>
